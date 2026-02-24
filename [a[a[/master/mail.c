ebcd808c866d7f80b8550931d5c39a6c3a86a9c5
vihtor1 <vitagrisin856@gmail.com>
2026-02-24 16:23:49 +0300

    Add files via upload

---
+++ [a[a[/master/mail.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "askname.h"
+int main(int argc, char **argv)
+{
+ char first[255], last[255];
+ askname(first, last);
+rintf("Hello, %s %s!\n", first, last);
+ return 0;
